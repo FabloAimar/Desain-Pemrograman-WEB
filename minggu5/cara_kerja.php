0d6b6405ce8ad0bae7ff61ce7158bf046bac7ea9
Fablo Aimar <fabloaimar03@gmail.com>
2022-03-22 22:36:27 +0700

    pengumpulan tugas praktikum minggu5

---
+++ minggu5/cara_kerja.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <p> kalimat dengan menggunakan HTML</p>
+        <?php
+            echo "kalimat dengan menggunakan PHP";
+        ?>
+    </body>
+</html>
